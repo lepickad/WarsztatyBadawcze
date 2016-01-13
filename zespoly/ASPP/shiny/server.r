b6e5c4b641020595c21cf1c751bed64cec042198
pkpytlak <pkpytlak@gmail.com>
2016-01-13 22:20:55 +0100

    ASPP_shiny

--- zespoly/ASPP/shiny/server.r
+++ zespoly/ASPP/shiny/server.r
@@ -1,56 +1,93 @@
-library(shiny)
+#library(survMisc)
 library(ggplot2)
-library(survMisc)
 library(stringi)
-sciezka = '/E/szkola/methylation/'
-setwd(sciezka)
-# zapis wszystkich inofrmacji w aplikacji po angielsku
-# pliki maja byc w tym miejscu gdzie sa pliki ui.
-# nie uzywac setwd
-
-# dodac stopien mrtyacji
-# zmiana zamiast mediane niech uzytkownik wybiera stopien metylacji
-# czy stpien powinine byc jeden globalny cyz nie
-# pokazac rozklad metylacji w roznych nowotrowach
-# rozklad biomakera w danym nowotworze np. hist
-
-# nazwe genu zawrzec w nazwie biomarkera
-
-# do nowotworow dodac mozlwisoc multiwyboru
-# ale zeby nie bylo to sklejenie tylko zeby senswone ocenic
-# kilka nowotowor naraz - cos zamiast KM
-
-# przygotowac jeden sensonwy zbior danych i wczytywac przed shnyServer()
-
-# wyslac na githuba 
-#dodac opis nowotoworow np. ze w danym nowotworze jest 500 obs
-
-# legenda do wykresu
-# jaki procent osob doyżje np 5 lat (survdiff)
+library(xtable)
 
 
-shinyServer(function(input, output) {
-  
- 
-# zmienic os y (skkala od )  
-  output$wykres <- renderPlot({
-    sciezka = stri_paste('pliki_rda/','all_', input$rak,'.rda')
-    zbior = get(load(sciezka))
-    # jak to zrobic aby wczytywac plik przed output, zeby nie trzeba bylo
-    # za kazdym razem wczytywac?
-    n=which(colnames(zbior)==input$marker)
-    if (length(n)!=0){
-      mediana <- median(zbior[,n], na.rm = TRUE)
-      test <- survfit(Surv(time, status == "dead")~(zbior[,n]>mediana), data = zbior)
-      autoplot(test,legend=FALSE)$plot +ylim(0,1)
-    }else{
-      cat("Wybrany przez Ciebie marker nie występuje w danym nowotrze")
-    }
-   
-  })
-  
-  output$podsumowanie <- renderPrint({
-    cat("Podsumowanie się robi...")
-  })
-  
-})
+load("data/data2.rda")
+load("data/signif_probes.rda")
+load("data/istotne2.rda")
+load("data/istotne3.rda")
+load("data/istotne4.rda")
+load("data/istotne5.rda")
+name = dir('km')
+for (i in seq_along(name)){
+  load(paste0('km/',name[i]))
+}
+
+shinyServer(
+   function(input, output) {
+      markerName <- reactive({unlist(stri_extract_all_regex(input$marker, "^.+(?=_)"))})
+      signifCancers <- reactive({as.character(signif_probes$cancer[signif_probes$probe==markerName()])})
+      chosen <- reactive({chosen <- signifCancers()[signifCancers() %in% input$cancer ==TRUE]})
+      signifMarkers <- reactive({signif_probes$probe[signif_probes$cancer==input$cancer_table]})
+      quantile=stats::quantile
+       output$histogram <- renderPlot({
+         if (length(input$cancer)==0){
+            return(NULL)
+         } else if (length(chosen())==0){
+            return(NULL)
+         } else {
+            #hist(data2$methylation[data2$marker==markerName() & (data2$cancer %in% chosen())])
+           ggplot2::ggplot(data2[data2$marker==markerName() & (data2$cancer %in% input$cancer),], 
+                   aes(x=methylation)) + geom_histogram(fill="red", alpha=0.5)
+         }
+      })
+      output$compHistograms <- renderPlot({
+         if (length(chosen())==0){
+            return(NULL)
+         } else {
+           ggplot2::ggplot(data2[data2$marker==markerName() & (data2$cancer %in% input$cancer),], 
+                   aes(x=methylation, fill=cancer, color=cancer)) + geom_histogram(alpha = 0.2)
+         }
+      })
+      output$boxplot <- renderPlot({
+         if (length(chosen())==0){
+            return(NULL)
+         } else {
+           quantile=stats::quantile
+           ggplot2::ggplot(data2[data2$marker==markerName() & (data2$cancer %in% input$cancer),], 
+                   aes(x=marker, y=methylation)) + ggplot2::geom_boxplot()
+         }
+      })
+      output$compBoxplots <- renderPlot({
+         if (length(chosen())==0){
+            return(NULL)
+         } else {
+           quantile=stats::quantile
+           ggplot2::ggplot(data2[data2$marker==markerName() & (data2$cancer %in% input$cancer),], 
+                   aes(x=marker, y=methylation, fill=cancer)) + 
+               geom_boxplot(position = position_dodge(width = 0.9))
+         }
+      })
+      output$signifCancer <- renderTable({
+         #data.frame(marker = signifMarkers())[1:20,]
+         head(data.frame(marker = signifMarkers()), 20)
+      })
+      
+      output$intersect2 <- renderTable({
+      data.frame(istotne2)
+      })
+      output$intersect3 <- renderTable({
+        data.frame(istotne3)
+      })
+      output$intersect4 <- renderTable({
+        data.frame(istotne4)
+      })
+      output$intersect5 <- renderTable({
+        data.frame(istotne5)
+      })
+      output$km <- renderPlot({
+        marker = stri_sub(input$marker2,1,10)
+        zbior = get(input$cancer2)
+        n = which(colnames(zbior)==marker)
+        x = sort(zbior[,n])
+        k = (input$methylation)/100
+        l = length(zbior[,n])
+        y = x[ceiling(k*l)]
+        test = survival::survfit(survival::Surv(time, status == "dead")~(zbior[,n]>y), data = zbior)
+        survMisc::autoplot(test,legend=FALSE)$plot +ylim(0,1)
+      })
+      
+   }
+)
--- zespoly/ASPP/shiny/ui.r
+++ zespoly/ASPP/shiny/ui.r
@@ -1,33 +1,150 @@
-library(shiny)
-library(ggplot2)
-library(survMisc)
-sciezka = '/E/szkola/methylation/'
-setwd(sciezka)
-markery = readLines('markery.txt' , warn = FALSE)
-markery = markery[-c(1:3)]
-shinyUI(fluidPage(
-  titlePanel("Profil metylacji"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("marker",
-                  label = "Wybierz marker",
-                  choices = markery),
-                  #slected = "cg27584469"),
-      #zmienic na wielokrotny wybor:
-      selectInput("rak",
-                  label = "Wybierz nowotwór",
-                  choices = (c('brca','coad', 'coadread', 'gbm', 'gbmlgg', 'kipan',
-                         'kirc', 'kirp', 'laml','luad','lusc', 'read','ucec','stes','stad','ov')),
-                  selected = "brca")
-    ),
-    
-    mainPanel(
-      p("Dane dotyczące danego markeru"),
-      br(),
-      tabsetPanel(
-        tabPanel("Wykres", plotOutput("wykres", width = 500)),
-        tabPanel("Podsumowanie", verbatimTextOutput("podsumowanie"))
-      )
-    )
-  )
+# setwd("data")
+# all_files <- list.files("data")
+# for(i in seq_along(all_files)){
+#    load(all_files[i])
+# }
+#setwd("data")
+load("data/markery_ist.rda")
+
+shinyUI(navbarPage("Methylation profile and survival analysis", 
+                   tabPanel("Introduction", 
+                            h1("Research workshops", align = "center"),
+                            h2("Methylation profile", align = "center"),
+                            h3("Adrianna Sudol & Pawel Pytlak", align = "center"),
+                            br(),
+                            p("Our application...")
+                            ),
+                   tabPanel("Distribution of methylation", 
+                            sidebarLayout(
+                               sidebarPanel(
+                                  selectInput("marker", 
+                                              label = "Choose a probe to analyse:",
+                                              choices = markery_ist,
+                                              selected = "cg00016968_RHOC"),
+                                  
+#                                   sliderInput("threshold", 
+#                                               label = "Choose a threshold for methylation:",
+#                                               min = 0, max = 1, value = 0.5),
+                                  
+                                  selectInput("cancer", label = "Choose cancer types:", 
+                                              choices = c("brca", "coad", "coadread", "gbm", 
+                                                          "gbmlgg", "kipan", "kirc", "kirp", 
+                                                          "laml", "luad", "lusc", "ov", "read", 
+                                                          "stad", "stes", "ucec"), 
+                                              selected = c("brca", "coad", "coadread", "gbm", 
+                                                           "gbmlgg", "kipan", "kirc", "kirp", 
+                                                           "laml", "luad", "lusc", "ov", "read", 
+                                                           "stad", "stes", "ucec"), 
+                                              multiple = TRUE, selectize = TRUE)
+                               ),
+                               
+                               mainPanel(
+                                  p("Statistical plots showing the distribution of methylation"),
+                                  br(),
+                                  tabsetPanel(
+                                     tabPanel("Cumulated histogram", 
+                                              "A histogram showing the distribution of methylation
+                                              for a given marker for all chosen cancer types", 
+                                              br(), 
+                                              plotOutput("histogram")), 
+                                     tabPanel("Comparison histograms", 
+                                              "Histograms showing the distribution of methylation 
+                                              for a given marker, by type of chosen cancer", 
+                                              br(), 
+                                              plotOutput("compHistograms")), 
+                                     tabPanel("Cumulated boxplot", 
+                                              "A boxplot showing the distribution of methylation
+                                              for a given marker for all chosen cancer types", 
+                                              br(), 
+                                              plotOutput("boxplot")), 
+                                     tabPanel("Comparison boxplots", 
+                                              "Boxplots showing the distribution of methylation
+                                              for a given marker, by type of chosen cancer", 
+                                              br(), 
+                                              plotOutput("compBoxplots"))
+                                    
+                                  )
+                           )
+                        )),
+                   tabPanel("Significant markers", 
+                            sidebarLayout(
+                               sidebarPanel(
+                                  selectInput("cancer_table", label = "Choose cancer types:", 
+                                              choices = c("brca", "coad", "coadread", "gbm", 
+                                                          "gbmlgg", "kipan", "kirc", "kirp", 
+                                                          "laml", "luad", "lusc", "ov", "read", 
+                                                          "stad", "stes", "ucec"), 
+                                              selected = "brca")
+                               ), 
+                               mainPanel(
+                                  p("Information about significant markers"), 
+                                  br(),
+                                  tabsetPanel(
+                                     tabPanel("Cancer-specific significant markers", 
+                                              "A table showing 20 most significant markers for 
+                                              a given cancer type", 
+                                              br(), 
+                                              tableOutput("signifCancer"))
+                                     
+                                     )
+                                  )
+                            )     
+                   ),
+
+                tabPanel("Common markers", 
+                         sidebarLayout(
+                           sidebarPanel(),
+                          mainPanel(
+                             p("Information about significant markers common for given cancer types"), 
+                             br(),
+                             tabsetPanel(
+                               tabPanel("At least 2 cancer types:", 
+                                      br(), 
+                                        tableOutput("intersect2")),
+                               tabPanel("At least 3 cancer types:", 
+                                        br(), 
+                                        tableOutput("intersect3")),
+                               tabPanel("At least 4 cancer types:", 
+                                        br(), 
+                                        tableOutput("intersect4")),
+                               tabPanel("Atleast 5 cancer types:", 
+                                        br(), 
+                                        tableOutput("intersect5"))
+                               
+                             )
+                           )
+                         )     
+                ),
+            
+                tabPanel("survival curves",
+                            sidebarLayout(
+                              sidebarPanel(
+                                selectInput("marker2", 
+                                            label = "Choose a probe to analyse:",
+                                            choices = markery_ist,
+                                            selected = "cg00016968_RHOC"),
+                                
+                                selectInput("cancer2", label = "Choose cancer types:", 
+                                                   choices = c("brca", "coad", "coadread", "gbm", 
+                                                               "gbmlgg", "kipan", "kirc", "kirp", 
+                                                               "laml", "luad", "lusc", "ov", "read", 
+                                                               "stad", "stes", "ucec"), 
+                                                   selected = "brca"),
+                                sliderInput("methylation",
+                                            "Stopien metylacji",min=1,max=99,value = 50)
+                              ),
+                              mainPanel(
+                                p("Plots depicting Kaplan-Meier survival curves for 
+                                  a given marker and a given cancer type. The division 
+                                  into two groups is based on the degree of methylation."),
+                                br(),
+                                tabsetPanel(
+                                  tabPanel("KM1", plotOutput("km", width = 500))
+                            
+                                )
+                              )
+                            ) 
+                  
+                )
 ))
+
