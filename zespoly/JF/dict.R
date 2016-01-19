a629fee01c426c8a7933c96df77fefd510891c61
katarzynaf <fk_katarzyna@gmail.com>
2016-01-19 23:01:48 +0100

    del

--- zespoly/JF/dict.R
+++
@@ -1,7 +0,0 @@
-# tworzenie slownika
-library(RTCGA.methylation)
-dict <- t(KIRP.methylation[2,])
-dict <- dict[-1,]
-dict <- data.frame(REF=names(dict), Gene_names=dict)
-rownames(dict) <- NULL
-save(dict, file="~/WarsztatyBadawcze/JF/dict.rda")
